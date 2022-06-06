b0ab38eb0d09f8f740f54a8df12d9755db6f1d85
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-06-06 22:01:31 +0600

    Update App.java

--- Thread/src/App.java
+++ Thread/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        A obj1 = new A();
+        B obj2 = new B();
+        C obj3 = new C();
+        obj1.start();
+        obj2.start();
+        obj3.start();
     }
 }
